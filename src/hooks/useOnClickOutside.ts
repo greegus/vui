1bae3f03817e0ac93c83e334d76b047c98e4708f
Matúš Duchoň <matus.duchon@gmail.com>
2023-02-22 14:51:35 +0100

    :lipstick:

--- src/hooks/useOnClickOutside.ts
+++ src/hooks/useOnClickOutside.ts
@@ -1,6 +1,6 @@
 import { onUnmounted, Ref } from 'vue'
 
-export function useOnClickOutside(element: Ref<HTMLElement | null>, callback: () => void) {
+export function useOnClickOutside(element: Ref<HTMLElement | undefined>, callback: () => void) {
   const handler = (e: MouseEvent) => {
     if (!element.value?.contains(e.target as HTMLElement)) {
       callback()
