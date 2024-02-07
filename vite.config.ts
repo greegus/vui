19928295ccafd087213ad14d6c0efea5a4457eb6
Matúš Duchoň <matus.duchon@gmail.com>
2024-02-08 00:07:48 +0100

    Remove unused vite-plugin-dts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import vue from '@vitejs/plugin-vue'
-import dts from 'vite-plugin-dts'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [vue(), dts()],
+  plugins: [vue()],
 
   build: {
     lib: {
