1cff3bff37b663add922120c2fa9fd65f291c6c5
Matúš Duchoň <matus.duchon@gmail.com>
2024-03-31 01:56:00 +0100

    Storybook 8 (#3)

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,15 +2,11 @@ const { resolve } = require('path')
 const { mergeConfig } = require('vite')
 
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: {
     name: '@storybook/vue3-vite',
     options: {}
-  },
-
-  features: {
-    'storyStoreV7': true
   },
 
   async viteFinal(config, { configType }) {
