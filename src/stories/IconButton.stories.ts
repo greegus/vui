3e29f3788529c10977b9ee6c82312a467c06a9b3
Matúš Duchoň <matus.duchon@gmail.com>
2024-04-01 10:58:48 +0200

    Add IconButton component

---
+++ src/stories/IconButton.stories.ts
@@ -0,0 +1,85 @@
+import { StoryObj, type Meta } from '@storybook/vue3'
+
+import IconButton from '../components/IconButton.vue'
+import { icons } from './assets/icons'
+
+export default {
+  title: 'Example/IconButton',
+  component: IconButton,
+  parameters: {
+    layout: 'centered',
+    docs: {
+      description: {
+        component: 'Standard IconButton'
+      }
+    }
+  },
+  argTypes: {
+    icon: {
+      control: 'select',
+      options: icons
+    },
+    size: {
+      control: 'select',
+      options: ['small', 'normal', 'large']
+    },
+    variant: {
+      control: 'select',
+      options: ['default', 'primary', 'secondary', 'danger', 'success']
+    },
+    block: {
+      control: 'boolean'
+    },
+    pill: {
+      control: 'boolean'
+    }
+  },
+  args: {
+    variant: 'primary',
+    icon: 'check'
+  }
+} as Meta<typeof IconButton>
+
+export const Default = {}
+
+export const Disabled: StoryObj<typeof IconButton> = {
+  args: { disabled: true }
+}
+
+export const Loading: StoryObj<typeof IconButton> = {
+  args: { loading: true }
+}
+
+export const Pill: StoryObj<typeof IconButton> = {
+  args: { pill: true }
+}
+
+export const Sizes: StoryObj<typeof IconButton> = {
+  render: (args) => ({
+    components: { IconButton },
+    setup: () => ({ args }),
+    template: `
+      <div style="display: flex; flex-flow: column; gap: 1rem;">
+        <IconButton v-bind="args" size="small" />
+        <IconButton v-bind="args" size="normal" />
+        <IconButton v-bind="args" size="large" />
+      </div>
+    `
+  })
+}
+
+export const Variants: StoryObj<typeof IconButton> = {
+  render: (args) => ({
+    components: { IconButton },
+    setup: () => ({ args }),
+    template: `
+      <div style="display: flex; flex-flow: column;  gap: 1rem;">
+        <IconButton v-bind="args" variant="default" />
+        <IconButton v-bind="args" variant="primary" />
+        <IconButton v-bind="args" variant="secondary" />
+        <IconButton v-bind="args" variant="danger" />
+        <IconButton v-bind="args" variant="success" />
+      </div>
+    `
+  })
+}
