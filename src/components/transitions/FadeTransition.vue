7430a6d7e60ef361e1cb8ff161239a7d0ef5ddc8
Matúš Duchoň <matus.duchon@gmail.com>
2024-04-03 08:53:18 +0200

    Add FadeTransition

---
+++ src/components/transitions/FadeTransition.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+withDefaults(
+  defineProps<{
+    duration: number
+  }>(),
+  {
+    duration: 300
+  }
+)
+</script>
+
+<template>
+  <Transition name="fade">
+    <slot />
+  </Transition>
+</template>
+
+<style lang="postcss">
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity v-bind(duration + 'ms');
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-to,
+.fade-leave-from {
+  opacity: 1;
+}
+</style>
