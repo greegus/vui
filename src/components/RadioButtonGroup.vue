089a0ffa0dcb1f6ef97bdc174be68045ae71e31e
Matúš Duchoň <matus.duchon@gmail.com>
2025-08-03 09:31:20 +0200

    Update RadioButtonGroup.vue

--- src/components/RadioButtonGroup.vue
+++ src/components/RadioButtonGroup.vue
@@ -7,6 +7,9 @@
       :outlined="!option.isSelected"
       :label="option.label"
       :disabled="$props.disabled || option.disabled"
+      :title="option.description"
+      :prefix-icon="option.icon"
+      :size="$props.size"
       @click="modelValue = option.value"
     />
   </ButtonGroup>
@@ -17,7 +20,7 @@ import { computed } from 'vue'
 
 import Button from '@/components/Button.vue'
 import ButtonGroup from '@/components/ButtonGroup.vue'
-import type { Extractor, Option, ValueParser } from '@/types'
+import type { Extractor, InputSize, Option, ValueParser } from '@/types'
 import { normalizeOptions } from '@/utils/normalizeOptions'
 
 const modelValue = defineModel<any>()
@@ -28,8 +31,10 @@ const props = defineProps<{
   optionValue?: Extractor
   optionDisabled?: Extractor
   optionIcon?: Extractor
+  optionDescription?: Extractor
   valueParser?: ValueParser<string>
   disabled?: boolean
+  size?: InputSize
 }>()
 
 const normalizedOptions = computed<Option[]>(() =>
@@ -39,6 +44,7 @@ const normalizedOptions = computed<Option[]>(() =>
       value: props.optionValue,
       label: props.optionLabel,
       disabled: props.optionDisabled,
+      description: props.optionDescription,
       icon: props.optionIcon
     },
     modelValue.value
