6802e6d33885bab48bd00307bcd94370571c0f68
Matúš Duchoň <matus.duchon@gmail.com>
2026-01-23 16:22:41 +0100

    Divider components

---
+++ src/components/Divider.vue
@@ -0,0 +1,48 @@
+<template>
+  <hr
+    class="Divider"
+    :class="{ 'Divider--vertical': orientation === 'vertical' }"
+  />
+</template>
+
+<script lang="ts" setup>
+withDefaults(defineProps<{
+  orientation?: "horizontal" | "vertical";
+}>(), {
+  orientation: "horizontal"
+});
+</script>
+
+<style scoped>
+.Divider {
+  --margin: var(--vuiii-divider-margin);
+  --color: var(--vuiii-divider-color);
+  --width: var(--vuiii-divider-width);
+
+  /* Reset default hr styles */
+  border: 0;
+  margin: 0;
+  padding: 0;
+
+  /* Horizontal (default) */
+  width: 100%;
+  height: 0;
+  border-top: var(--width) solid var(--color);
+  margin-top: var(--margin);
+  margin-bottom: var(--margin);
+}
+
+.Divider--vertical {
+  /* Vertical orientation */
+  display: inline-block;
+  width: 0;
+  height: 100%;
+  border-top: 0;
+  border-left: var(--width) solid var(--color);
+  margin-top: 0;
+  margin-bottom: 0;
+  margin-left: var(--margin);
+  margin-right: var(--margin);
+  vertical-align: middle;
+}
+</style>
