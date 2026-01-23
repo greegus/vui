1b9ee8a88734c71e33d64c7fc6af0eaedd355a18
Matúš Duchoň <matus.duchon@gmail.com>
2026-01-23 14:50:03 +0100

    Upgrade storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,4 @@
+// This file has been automatically migrated to valid ESM format by Storybook.
 import { resolve, dirname } from "path";
 import { fileURLToPath } from "url";
 import { mergeConfig } from "vite";
